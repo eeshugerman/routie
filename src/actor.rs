79c6f0be67c844e6962d23fcfc18b471a9360470
Elliott Shugerman <eeshugerman@gmail.com>
2022-12-05 23:17:54 -0500

    actor context

--- src/actor.rs
+++ src/actor.rs
@@ -2,6 +2,8 @@ extern crate nalgebra;
 
 use nalgebra::Point2;
 
+use crate::road::{Direction, JunctionLaneContext, PosParam, SegmentContext, SegmentLaneContext};
+
 #[allow(dead_code)]
 pub struct Actor {
     id: i32,
@@ -9,4 +11,14 @@ pub struct Actor {
     max_speed: f64,
     // route: Option<Vec<RouteStep>>,
     // agenda: Vec<AgendaStep>
+}
+
+pub enum ActorLaneContext<'a> {
+    Segment(SegmentLaneContext<'a>),
+    Junction(JunctionLaneContext<'a>),
+}
+
+pub enum ActorContext<'a> {
+    OnRoad { pos_param: PosParam, lane_ctx: ActorLaneContext<'a> },
+    OffRoad { pos_param: PosParam, segment_ctx: SegmentContext<'a>, segment_side: Direction },
 }
