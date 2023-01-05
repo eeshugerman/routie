6722f7f396be93bd19bc4e57e8c0f7f56983d16b
Elliott Shugerman <eeshugerman@gmail.com>
2023-01-05 12:24:48 -0700

    build movie

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,16 @@ mod constants;
 mod draw;
 mod error;
 mod road;
-mod spatial;
 mod simulate;
-
-use std::fs::File;
+mod spatial;
 
 extern crate log;
 
+use std::io::Write;
+
 use actor::Agendum;
-use cairo::{Format, ImageSurface};
 use draw::IMAGE_SIZE;
 use nalgebra::Point2;
-use simulate::advance;
 
 fn main() {
     env_logger::init();
@@ -33,15 +31,15 @@ fn main() {
     let (s1_id, s1) = network.add_segment(j1, j2);
     s1.add_lane(road::Direction::Backward);
     s1.add_lane(road::Direction::Forward);
-    s1.add_actor(0.25, road::Direction::Backward, vec![
-        Agendum::TravelTo(actor::LocationOffRoad {
+    s1.add_actor(
+        0.25,
+        road::Direction::Backward,
+        vec![Agendum::TravelTo(actor::LocationOffRoad {
             segment_id: s1_id,
             segment_side: road::Direction::Backward,
-            pos_param: 0.75
-        })
-    ]);
-
-
+            pos_param: 0.75,
+        })],
+    );
 
     let (_, s2) = network.add_segment(j3, j4);
     s2.add_lane(road::Direction::Backward);
@@ -57,23 +55,32 @@ fn main() {
 
     network.connect_junctions();
 
-
-    for result in std::fs::read_dir("./frames/").unwrap() {
-        match result {
-            Ok(entry) => if entry.file_name() != ".gitkeep" {
-                std::fs::remove_file(entry.path()).unwrap()
-            },
-            Err(_) => (),
-        }
-    }
-
-    for i in 0..15 {
+    // build movie
+    // based on https://gist.github.com/tetsu-koba/14083c6705b69017bbc7fb97602f610a
+    let _ = std::fs::remove_file("./out.mp4");
+    let mut ffmpeg = std::process::Command::new("/bin/sh")
+        .args(&["-c", &format!(
+            "ffmpeg -r {framerate} -f rawvideo -pix_fmt bgra -s {width}x{height} -i pipe: -pix_fmt yuv420p -r {framerate} -y out.mp4",
+            width = draw::IMAGE_SIZE,
+            height = draw::IMAGE_SIZE,
+            framerate = 1
+        )])
+        .stdin(std::process::Stdio::piped())
+        .spawn()
+        .expect("failed to execute child");
+    let mut frames: Vec<cairo::ImageSurfaceDataOwned> = Vec::new();
+    for _i in 0..15 {
+        println!("{}", _i);
         // TODO: redraw actors only
-        let surface = ImageSurface::create(Format::ARgb32, IMAGE_SIZE, IMAGE_SIZE).unwrap();
-        let mut file = File::create(format!("./frames/{}.png", i)).unwrap();
+        let surface =
+            cairo::ImageSurface::create(cairo::Format::ARgb32, IMAGE_SIZE, IMAGE_SIZE).unwrap();
         draw::draw(&surface, &network);
-        surface.write_to_png(&mut file).unwrap();
-        network = advance(network);
+        frames.push(surface.take_data().unwrap());
+        network = simulate::advance(network);
     }
-
+    let ffmpeg_stdin = ffmpeg.stdin.as_mut().unwrap();
+    for frame in frames {
+        ffmpeg_stdin.write_all(&frame).unwrap();
+    }
+    ffmpeg.wait().expect("child process wasn't running");
 }
